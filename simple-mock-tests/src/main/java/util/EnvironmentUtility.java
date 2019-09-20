0581dfa7e4fb97eabeb4f4f61e8e3330b38025c3
Sean Pike <pike.sean@gmail.com>
2019-09-20 15:18:05 +0100

    Add simple mocked api test

---
+++ simple-mock-tests/src/main/java/util/EnvironmentUtility.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class EnvironmentUtility {
+    private static Properties properties;
+
+    private static void loadPropertiesFromFile() {
+        try {
+            InputStream in = new FileInputStream("../../simple-mock-tests/src/main/resources/simple.properties");
+            properties = new Properties();
+            properties.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String propertyName) {
+        if (properties == null) {
+            loadPropertiesFromFile();
+        }
+        return properties.getProperty(propertyName);
+    }
+}
