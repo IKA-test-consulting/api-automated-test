0809eb5135a213c4d3355a2ac19ae6a0acc5298f
Sean Pike <pike.sean@gmail.com>
2019-10-16 11:38:44 +0100

    Add Auth Service tests

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -1,14 +1,18 @@
 package service;
 
 import client.ApiRestClient;
+import io.restassured.response.Response;
 import mock.AuthMock;
 import util.EnvironmentUtility;
 
 import java.util.Map;
 
 public class AuthService {
+    private static final String CLIENT_PASSWORD_HEADER = "x-client-password";
+    private static final String CLIENT_ID_HEADER = "x-client-id";
     private String authEndPoint = EnvironmentUtility.getProperty("simple.service.auth");
     private String service = EnvironmentUtility.getProperty("simple.service.url");
+
 
     public AuthService() {
         new AuthMock(service, authEndPoint);
@@ -16,8 +20,16 @@ public class AuthService {
 
     public String getToken() {
         return new ApiRestClient()
-                .getAuth(service + authEndPoint, Map.of("x-client-id", "fakeId", "x-client-password", "fakePassword"))
+                .getAuth(service + authEndPoint, Map.of(CLIENT_ID_HEADER, "fakeId", CLIENT_PASSWORD_HEADER, "fakePassword"))
                 .jsonPath()
                 .getString("token");
+    }
+
+    public Response get(String clientId, String clientPassword) {
+        return new ApiRestClient().getAuth(service + authEndPoint, Map.of(CLIENT_ID_HEADER, clientId, CLIENT_PASSWORD_HEADER, clientPassword));
+    }
+
+    public Response getMissingCredentials() {
+        return new ApiRestClient().getAuth(service + authEndPoint, Map.of(CLIENT_ID_HEADER, "missingHeader"));
     }
 }
