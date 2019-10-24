90c98cb0d6df939e3653ed8b80c23d908afc1fda
Sean Pike <pike.sean@gmail.com>
2019-10-24 16:31:44 +0100

    Use properties file for auth and client services

--- junit-tests/src/main/java/utility/EnvironmentConstants.java
+++ junit-tests/src/main/java/utility/EnvironmentConstants.java
@@ -1,6 +1,10 @@
 package utility;
 
+import util.PropertiesReader;
+
 public class EnvironmentConstants {
-    public static final String AUTH_SERVICE = "/auth";
-    public static final String HOST = "http://fake.123";
+    private static PropertiesReader properties = new PropertiesReader("/src/main/resources/", "junit-environment.properties");
+    public static final String AUTH_SERVICE = properties.getProperty("AUTH_SERVICE");
+    public static final String HOST = properties.getProperty("HOST");
+    public static final String CLIENT_SERVICE = properties.getProperty("CLIENT_SERVICE");
 }
